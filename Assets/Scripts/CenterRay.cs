0b9f200ef4ca0289511d78d554b9f1a013e181c9
百科おじさん <tftf161107@gmail.com>
2020-07-06 16:20:44 +0900

    [add] pickUpController

--- Assets/Scripts/CenterRay.cs
+++ Assets/Scripts/CenterRay.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CenterRay : MonoBehaviour
 {
+    PickUpController pickUpController;
+
     string state = "";
     Vector3 center = new Vector3(Screen.width/2, Screen.height/2);
     RaycastHit hit;
     int distance = 3;
+
+    void Start() {
+        pickUpController = GameObject.Find("GameDirector").GetComponent<PickUpController>();
+    }
 
     void Update() {
         Ray ray = Camera.main.ScreenPointToRay(center);
@@ -20,12 +26,20 @@ public class CenterRay : MonoBehaviour
             if (state != tag && tag != "Untagged") {
                 state = tag;
                 Debug.Log($"{tag}を見つけました");
+                pickUpController.TogglePickUpButton(
+                    active: true,
+                    newText: $"{tag}を拾う",
+                    newTargetTag: tag);
             }
         } else {
             // オブジェクトと当たらなかったとき
             if (state != "") {
                 state = "";
                 Debug.Log("オブジェクトを見失いました");
+                pickUpController.TogglePickUpButton(
+                    active: false,
+                    newText: "",
+                    newTargetTag: "");
             }
         }
     }
