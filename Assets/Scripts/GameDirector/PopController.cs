2d2c60f3063cdde69c3fc5199cf9a4bc72b0a7d5
百科おじさん <tftf161107@gmail.com>
2020-08-10 03:18:26 +0900

    [add: popボタンを追加

---
+++ Assets/Scripts/GameDirector/PopController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopController : MonoBehaviour
+{
+    DevLog devLog;
+
+    void Start()
+    {
+        devLog = GetComponent<DevLog>();
+    }
+
+    public void Pop() {
+        devLog.SendLog("Flutterさんpopしてください");
+    }
+}
