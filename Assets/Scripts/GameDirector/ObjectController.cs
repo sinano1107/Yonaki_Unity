c1308f3c38bcc6de613abcadd1a7fed9471ae5be
百科おじさん <tftf161107@gmail.com>
2020-08-18 19:16:49 +0900

    [fix] 忘れていたDestroyObjectのnextを追加

--- Assets/Scripts/GameDirector/ObjectController.cs
+++ Assets/Scripts/GameDirector/ObjectController.cs
@@ -136,6 +136,7 @@ public class ObjectController : MonoBehaviour
         {
             Destroy(target);
         }
+        UnityMessageManager.Instance.SendMessageToFlutter("next");
     }
 
     // AssetBundleのアンロード
