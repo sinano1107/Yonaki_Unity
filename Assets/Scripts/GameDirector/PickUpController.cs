3edabe9d3126ab39831ec3584e2388b6e9677c6a
百科おじさん <tftf161107@gmail.com>
2020-10-26 13:04:30 +0900

    [update] CH_オブジェクトのpickupの際、親オブジェクトを削除するようにアップデート

--- Assets/Scripts/GameDirector/PickUpController.cs
+++ Assets/Scripts/GameDirector/PickUpController.cs
@@ -31,6 +31,9 @@ public class PickUpController : MonoBehaviour
         }
     
     public void PickUp() {
+        // CH_のついたチルドレンオブジェクトの場合親を削除
+        if (targetTag.Substring(0, 3) == "CH_") targetTag = targetTag.Substring(3);
+
         Destroy(GameObject.FindGameObjectWithTag(targetTag));
         Destroy(GameObject.FindGameObjectWithTag("TargetIndicator"));
 
